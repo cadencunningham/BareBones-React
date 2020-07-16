49d2bf0685f0add8411e97f5ce26d66e0305f712
Caden Cunningham <xcadencunningham@gmail.com>
2020-07-15 21:49:46 -0500

    added react router with a base path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import React from "react";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './app.css';
 
 const App = () => {
     return(
-      <div>
-        <h1> This is A Bare Bones React App</h1>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" component={()=><h1> This is A Bare Bones React App</h1>}/>
+        </div>
+      </Router>
     );
 }
 
