0d9c18053b146e68a3d9803aa3418cdc76ec6e13
Caden Cunningham <xcadencunningham@gmail.com>
2020-08-02 17:42:33 -0500

    Base Project Setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js", 
+    entry: "./src/index.tsx", 
     mode: "development",  
     module : {
         rules:[ 
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "awesome-typescript-loader",
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
             {
                 test: /\.(svg|png|jpe?g|gif)$/,
@@ -27,7 +36,7 @@ module.exports = {
             },
         ]
     },
-    resolve: {extensions: ["*", ".js", ".jsx"]},
+    resolve: {extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".json"]},
     output:{
         filename: "bundle.js",
         chunkFilename: '[id].js',
@@ -40,7 +49,7 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
+            template: path.resolve(__dirname,'src', 'index.html'),
             filename: 'index.html',
             inject: 'body'
         })
